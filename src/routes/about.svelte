31ef079888727707da7f9baf38aad45bfe26313a
metonymize-kripa <kripa@metonymize.com>
2021-02-20 06:21:15 -0600

    Setting up basics

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,6 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About 💎 Oracle</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Who is the Oracle? What is the Matrix? The truth is, we no longer know.</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,121 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
+<script>
+	import Eliza from 'elizabot';
+	import { beforeUpdate, afterUpdate } from 'svelte';
+	import { onMount } from "svelte";
+	const apiURL = "https://serpapi.com/search?q=coffee";
+	let data = [];
+	
+	let div;
+	let autoscroll;
+	
+	onMount(async function() {
+        const response = await fetch(apiURL);
+        data = await response.json();
+    	});
+
+	beforeUpdate(() => {
+		autoscroll = div && (div.offsetHeight + div.scrollTop) > (div.scrollHeight - 20);
+	});
+
+	afterUpdate(() => {
+		if (autoscroll) div.scrollTo(0, div.scrollHeight);
+	});
+
+	const eliza = new Eliza();
+	
+	let comments = [{ author: 'eliza', text: eliza.getInitial() }];
+	
+	function runAPI(query) {
+		/*
+		fetch("https://serpapi.com/search?q=coffee"+query)
+			.then(d => d.text())
+			.then(d => (data = JSON.parse(d)));
+		*/
+		return 'time for '+query
 	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+	function handleKeydown(event) {
+		if (event.key === 'Enter') {
+			const text = event.target.value;
+			if (!text) return;
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+			comments = comments.concat({
+				author: 'user',
+				text
+			});
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+			event.target.value = '';
 
-	p {
-		margin: 1em auto;
-	}
+			const reply = eliza.transform(text);
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
+			setTimeout(() => {
+				comments = comments.concat({
+					author: 'eliza',
+					text: '...',
+					placeholder: true
+				});
+
+				setTimeout(() => {
+					comments = comments.filter(comment => !comment.placeholder).concat({
+						author: 'eliza',
+						text: reply
+					});
+				}, 500 + Math.random() * 500);
+			}, 200 + Math.random() * 200);
 		}
+	}
+</script>
+
+<style>
+	.chat {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		width: 100%;
+	}
+
+	.scrollable {
+		flex: 1 1 auto;
+		border-top: 1px solid #eee;
+		margin: 0 0 0.5em 0;
+		overflow-y: auto;
+	}
+
+	article {
+		margin: 0.5em 0;
+	}
+
+	.user {
+		text-align: right;
+	}
+
+	span {
+		padding: 0.5em 1em;
+		display: inline-block;
+	}
+
+	.eliza span {
+		background-color: #eee;
+		border-radius: 1em 1em 1em 0;
+	}
+
+	.user span {
+		background-color: #0074D9;
+		color: white;
+		border-radius: 1em 1em 0 1em;
 	}
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
+<div class="chat">
+	<h1>BET: Basic Eliza Test</h1>
 
-<h1>Great success!</h1>
+	<div class="scrollable" bind:this={div}>
+		{#each comments as comment}
+			<article class={comment.author}>
+				<span>{comment.text}</span>
+			</article>
+		{/each}
+	</div>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<input on:keydown={handleKeydown}>
+</div>
